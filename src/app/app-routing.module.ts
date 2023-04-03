1d375e804a63bae030083c6e6bd47c9e0bb067d1
ccristianss <cristian.silva9402@gmail.com>
2023-04-03 14:46:47 -0500

    Se crea proyecto en Angular y se modifica la estru ra principal del usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//components
+
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { AlquileresComponent } from './components/alquileres/alquileres.component';
+import { HistorialComponent } from './components/historial/historial.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
+
+const routes: Routes = [
+  {path:'', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { AlquileresComponent } from './components/alquileres/alquileres.component';
+import { HistorialComponent } from './components/historial/historial.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    MainComponent,
+    NavComponent,
+    PerfilComponent,
+    AlquileresComponent,
+    HistorialComponent,
+    FavoritosComponent
   ],
   imports: [
     BrowserModule,
